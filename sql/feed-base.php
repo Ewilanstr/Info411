e92f3848bb59d50b0301c9171ee91463a1f72a43
Camille <camille.struffi@gmail.com>
2025-03-14 19:22:32 +0100

    try to fix it

--- sql/feed-base.php
+++ sql/feed-base.php
@@ -1,5 +1,5 @@
 <?php
-include("../include/connexion.php")
+include("../include/connexion.php");
 $json_data = file_get_contents('feed.json'); // Read the JSON file
 $data = json_decode($json_data, true);
 
@@ -16,7 +16,7 @@ foreach($arr as $item) {
     if($item["categorie"] == 2){
         $nom = $item["title"] ;
         $img = $item["image"] ;
-        $sql = 'INSERT INTO `jeux` (`id`, `nom`, `Image`) VALUES (NULL, '$nom', '$img');';
+        $sql = "INSERT INTO `jeux` (`id`, `nom`, `Image`) VALUES (NULL, '$nom', '$img');";
         mysqli_query($conn, $sql);
         var_dump($nom);
     }
