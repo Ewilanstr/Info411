73af081bdc2957be24bb6c923eea371f723f0699
Bluz0 <amouats5@gmail.com>
2025-04-18 14:02:31 +0200

    modif sh

--- maj-site-web.sh
+++ maj-site-web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git pull https://github.com/Ewilanstr/Info411.git
-docker compose down
-docker-compose up -d
+sudo git pull https://github.com/Ewilanstr/Info411.git
+sudo docker compose down
+sudo docker-compose up -d
